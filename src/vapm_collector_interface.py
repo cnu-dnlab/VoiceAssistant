907f9234e9952168c68daf790ebd030efbd5039e
munhyunsu <munhyunsu@gmail.com>
2018-12-27 15:23:29 +0900

    pcap collector

--- src/vapm_collector_interface.py
+++ src/vapm_collector_interface.py
@@ -1,19 +1,30 @@
-class VAMPInterface(object):
-    def __init__(self):
-        pass
+import os
+import shutil
+
+
+class VAPMInterface(object):
+    def __init__(self, tmp_path):
+        self.tmp_filepath = os.path.abspath(tmp_path)
+        self._create_tmp_dir()
+    
+    def _create_tmp_dir(self):
+        os.makedirs(os.path.dirname(self.tmp_filepath), exist_ok=True)
 
     def clear_tmp(self):
-        pass
+        if os.path.exists(self.tmp_filepath):
+            os.remove(self.tmp_filepath)
     
-    def archive_tmp(self):
-        pass
+    def archive_tmp(self, target):
+        target_filepath = os.path.abspath(target)
+        shutil.copyfile(self.tmp_filepath, target_filepath)
 
-    def start_collect(self):
-        self.clear_tmp(self):
+    def start_collect(self, target):
+        self.clear_tmp()
         try:
             self._do_collect()
-        except:
-            self.archive_tmp()
+        except KeyboardInterrupt:
+            self.archive_tmp(target)
+        self.clear_tmp()
 
     def _do_collect(self):
         raise NotImplementedError
---
+++ src/vapm_collector_pcap.py
@@ -0,0 +1,22 @@
+import subprocess
+
+from vapm_collector_interface import VAPMInterface
+
+
+class VAPMCollectorPcap(VAPMInterface):
+    def __init__(self, tmp_path, router_ip):
+        super().__init__(tmp_path)
+        self.router_ip = router_ip
+
+    def _do_collect(self):  # override
+        command = ('ssh root@{0} "tcpdump -i br-lan -s 0 -U -w - '
+                   '\(not port 22\)" > {1}').format(self.router_ip,
+                                                    self.tmp_filepath)
+        self.popen = subprocess.run(command, shell=True)
+
+
+if __name__ == '__main__':
+    vapm_pcap = VAPMCollectorPcap('/tmp/pcap/router.pcap',
+                                  '192.168.1.1')
+    vapm_pcap.start_collect('./temp.pcap')
+
