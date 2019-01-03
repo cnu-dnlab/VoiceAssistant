dc4219b51afde76dc6b9b58003c3575683d348f3
kimSooHyun950921 <soohyunkim950921@gmail.com>
2019-01-03 18:56:30 +0900

    modify dependency-checker

--- src/dependency_checker.py
+++ src/dependency_checker.py
@@ -1,11 +1,33 @@
 import subprocess
 
+import pkg_resources
+from pkg_resources import DistributionNotFound
+from pkg_resources import VersionConflict
 
-def dependency_checker():
-    check_module_list = ['SoundFile', 'sounddevice', 'opencv-python']
-    subprocess.run("pip freeze > requirements.txt", shell=True)
-    with open('./requirements.txt') as pip_list_file:
-        installed_module_list = pip_list_file.read()
-        for module in check_module_list:
-            if not module in installed_module_list:
-                raise ImportError
+DEPENENCIES = ['SoundFile>=0.10.2',
+               'sounddevice>=0.3.0',
+               'opencv-python>=3.4.4.0',
+               'snvf']
+BINARIES = ["abc"]
+CHECK_MSG = " --help"
+
+def module_checker():
+    not_installed_modules = []
+    for dependency in DEPENENCIES:
+        try:
+            pkg_resources.require(dependency)
+        except DistributionNotFound:
+            not_installed_modules.append(dependency)
+        except VersionConflict:
+            not_installed_modules.append(dependency)
+
+    return not_installed_modules
+
+def binary_checker():
+    not_installed_binaries = []
+    for binary in BINARIES:
+        binary_check_msg = binary + CHECK_MSG
+        exit_code = subprocess.getstatusoutput(binary_check_msg)
+        if exit_code[0] != 0:
+            not_installed_binaries.append(binary)
+    return not_installed_binaries
