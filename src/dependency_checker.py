e7ddbeccdda97762a3b53192fbfc27a4e80aaa00
kimSooHyun950921 <soohyunkim950921@gmail.com>
2019-01-05 00:11:14 +0900

    add module:make binary list from binary list file

--- src/dependency_checker.py
+++ src/dependency_checker.py
@@ -1,15 +1,21 @@
 import subprocess
+import csv
 
 import pkg_resources
 from pkg_resources import DistributionNotFound
 from pkg_resources import VersionConflict
 
-BINARIES = ["abc"]
-CHECK_MSG = " --help"
+def make_binary_list():
+    binaries = list()
+    with open('binary_list.csv', 'r') as bin_list_file:
+        reader = csv.reader(bin_list_file, delimiter=',')
+        for _, row in enumerate(reader):
+            binaries.append(row)
+    return binaries
 
 def make_dependencies_list():
     dependencies = list()
-    with open('requirements.txt') as require_file:
+    with open('requirements.txt', 'r') as require_file:
         while True:
             line = require_file.readline()
             if not line:
@@ -31,9 +37,11 @@ def module_checker():
 
 def binary_checker():
     not_installed_binaries = []
-    for binary in BINARIES:
-        binary_check_msg = binary + CHECK_MSG
+    binaries = make_binary_list()
+    for binary in binaries:
+        binary_check_msg = binary[0] +" "+binary[1]
         exit_code = subprocess.getstatusoutput(binary_check_msg)
         if exit_code[0] != 0:
             not_installed_binaries.append(binary)
     return not_installed_binaries
+
