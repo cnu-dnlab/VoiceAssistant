87d2600acbb0d0f7f06f1b86b106f28737cac27e
HyungJiny <hyungjin.lee1004@gmail.com>
2019-01-22 11:32:16 +0900

    명령어 저장 json 양식 변경

--- src/commandParser.py
+++ src/commandParser.py
@@ -3,14 +3,14 @@ import time
 import json
 
 from gtts import gTTS
-
+from datetime import datetime
 
 class CommandSpeeker():
 	def __init__(self, speaker, json_path):
 		self.speaker = speaker
 		self.json_file = open(json_path, "r")
 		self.command_json = json.load(self.json_file)
-		self.language = self.command_json['language']
+		self.language = self.command_json[self.speaker]['language']
 		self.tts = None
 		self.filename = "command.mp3"
 
@@ -20,31 +20,31 @@ class CommandSpeeker():
 		# if you don't install 'mpg123', should install by apt
 		os.system("mpg123 " + self.filename)
 	
-	def _do_command(self, category, command_number):
-		self._play_command_to_mp3(self.command_json['call'])
+	def _do_command(self, category):
+		self._play_command_to_mp3(self.command_json[self.speaker]['call'])
 		time.sleep(0.7)
-		self._play_command_to_mp3(self.command_json["command"][category][command_number])
+		self._play_command_to_mp3(self.command_json[self.speaker]['command'][category])
 		self.json_file.close()
 	
-	def start_command(self, category, command_number, write_record):
+	def start_command(self, category, write_record):
 		start_time = time.time()
 		try:
-			self._do_command(category, command_number)
+			self._do_command(category)
 			if write_record: time.sleep(60)
 		except KeyboardInterrupt:
 			duration = time.time() - start_time
-			if write_record: self._write_record(category, command_number, duration)
+			if write_record: self._write_record(category, duration)
 			os.remove(self.filename)
 			self.json_file.close()
 	
-	def _write_record(self, category, command_number, duration):
-		record_file = './test_record.csv'
+	def _write_record(self, category, duration):
+		record_file = './'+datetime.today().strftime('%y%m%d')+'_test_record.csv'
 		isfrist = True
 		if os.path.exists(record_file): isfrist = False
 		with open(record_file, 'a') as file:
 			if isfrist: file.write('speeker, category, command, time\n')
 			file.write(self.speaker+', '+category+', '+\
-				self.command_json['command'][category][command_number]+', '+\
+				self.command_json[self.speaker]['command'][category]+', '+\
 				str(duration)+'\n')
 
 
@@ -64,10 +64,6 @@ if __name__=="__main__":
 						help='kind of command category',
 						type=str,
 						required=True)
-	parser.add_argument('-n', '--command-number',
-						help='the number of command as json',
-						type=int,
-						default=0)
 	parser.add_argument('-w', '--write-record',
 						help='write test record',
 						type=bool,
@@ -76,6 +72,5 @@ if __name__=="__main__":
 
 	commandspeeker = CommandSpeeker(ARGS.speeker, ARGS.json_path)
 	commandspeeker.start_command(ARGS.category, 
-								ARGS.command_number,
 								ARGS.write_record)
 
