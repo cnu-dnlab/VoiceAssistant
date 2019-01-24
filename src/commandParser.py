75fc72003da04fc68beb945377e2344daac0601c
HyungJiny <hyungjin.lee1004@gmail.com>
2019-01-24 23:57:43 +0900

    명령어 json 양식을 스피커 정보와 명령어 정보로 분할

--- src/commandParser.py
+++ src/commandParser.py
@@ -6,13 +6,26 @@ from gtts import gTTS
 from datetime import datetime
 
 class CommandSpeeker():
-	def __init__(self, speaker, json_path):
+	def __init__(self, speaker, speeker_json_path, command_json_path):
 		self.speaker = speaker
-		self.json_file = open(json_path, "r")
-		self.command_json = json.load(self.json_file)
-		self.language = self.command_json[self.speaker]['language']
+		self.language, self.call, self.functions = self._read_speeker_json(speeker_json_path)
+		self.commands = self._read_command_json(command_json_path)
 		self.tts = None
 		self.filename = "command.mp3"
+	
+	def _read_speeker_json(self, speeker_json_path):
+		with  open(speeker_json_path, 'r') as speeker_json_file:
+			speeker_json = json.load(speeker_json_file)
+			language = speeker_json[self.speaker]['language']
+			call = speeker_json[self.speaker]['call']
+			functions = speeker_json[self.speaker]['function']
+		return (language, call, functions)
+
+	def _read_command_json(self, command_json_path):
+		with open(command_json_path, 'r') as command_json_file:
+			command_json = json.load(command_json_file)
+			commands = command_json[self.language]
+		return commands
 
 	def _play_command_to_mp3(self, text):
 		self.tts = gTTS(text, self.language)
@@ -21,31 +34,29 @@ class CommandSpeeker():
 		os.system("mpg123 " + self.filename)
 	
 	def _do_command(self, category):
-		self._play_command_to_mp3(self.command_json[self.speaker]['call'])
-		time.sleep(0.7)
-		self._play_command_to_mp3(self.command_json[self.speaker]['command'][category])
-		self.json_file.close()
+		self._play_command_to_mp3(self.call)
+		#time.sleep(0.5) # if you need
+		for command in self.commands[category]:
+			self._play_command_to_mp3(command)
+			time.sleep(1)
 	
 	def start_command(self, category, write_record):
 		start_time = time.time()
 		try:
 			self._do_command(category)
-			if write_record: time.sleep(60)
+			if write_record: time.sleep(30)
 		except KeyboardInterrupt:
 			duration = time.time() - start_time
 			if write_record: self._write_record(category, duration)
 			os.remove(self.filename)
-			self.json_file.close()
 	
 	def _write_record(self, category, duration):
 		record_file = './'+datetime.today().strftime('%y%m%d')+'_test_record.csv'
 		isfrist = True
 		if os.path.exists(record_file): isfrist = False
 		with open(record_file, 'a') as file:
-			if isfrist: file.write('speeker, category, command, time\n')
-			file.write(self.speaker+', '+category+', '+\
-				self.command_json[self.speaker]['command'][category]+', '+\
-				str(duration)+'\n')
+			if isfrist: file.write('speeker\tcategory\ttime\n')
+			file.write(self.speaker+'\t'+category+'\t'+str(duration)+'\n')
 
 
 if __name__=="__main__":
@@ -57,11 +68,15 @@ if __name__=="__main__":
 						type=str,
 						required=True)
 	parser.add_argument('-j', '--json-path',
-						help='command json file path',
+						help='the path of json file has speeker information',
 						type=str,
 						required=True)
-	parser.add_argument('-c', '--category',
-						help='kind of command category',
+	parser.add_argument('-c', '--command-json-path',
+						help="the path of json file has commands",
+						type=str,
+						required=True)
+	parser.add_argument('-f', '--function',
+						help='kind of command function',
 						type=str,
 						required=True)
 	parser.add_argument('-w', '--write-record',
@@ -70,7 +85,6 @@ if __name__=="__main__":
 						default=False)
 	ARGS = parser.parse_args()
 
-	commandspeeker = CommandSpeeker(ARGS.speeker, ARGS.json_path)
-	commandspeeker.start_command(ARGS.category, 
-								ARGS.write_record)
+	commandspeeker = CommandSpeeker(ARGS.speeker, ARGS.json_path, ARGS.command_json_path)
+	commandspeeker.start_command(ARGS.function, ARGS.write_record)
 
