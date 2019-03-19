76cbf4bdebd481a0105d680f664d2eb974d9f524
Hyungjin Lee <hyungjin.lee1004@gmail.com>
2019-03-19 23:36:31 +0900

    Update vapm_command_parser.py
    오타 수정

--- src/vapm_command_parser.py
+++ src/vapm_command_parser.py
@@ -5,20 +5,20 @@ import json
 from gtts import gTTS
 from datetime import datetime
 
-class CommandSpeeker():
-	def __init__(self, speaker, speeker_json_path, command_json_path):
+class CommandSpeaker():
+	def __init__(self, speaker, speaker_json_path, command_json_path):
 		self.speaker = speaker
-		self.language, self.call, self.functions = self._read_speeker_json(speeker_json_path)
+		self.language, self.call, self.functions = self._read_speeker_json(speaker_json_path)
 		self.commands = self._read_command_json(command_json_path)
 		self.tts = None
 		self.filename = "command.mp3"
 	
-	def _read_speeker_json(self, speeker_json_path):
-		with  open(speeker_json_path, 'r') as speeker_json_file:
-			speeker_json = json.load(speeker_json_file)
-			language = speeker_json[self.speaker]['language']
-			call = speeker_json[self.speaker]['call']
-			functions = speeker_json[self.speaker]['function']
+	def _read_speeker_json(self, speaker_json_path):
+		with  open(speaker_json_path, 'r') as speaker_json_file:
+			speaker_json = json.load(speaker_json_file)
+			language = speaker_json[self.speaker]['language']
+			call = speaker_json[self.speaker]['call']
+			functions = speaker_json[self.speaker]['function']
 		return (language, call, functions)
 
 	def _read_command_json(self, command_json_path):
@@ -37,9 +37,12 @@ class CommandSpeeker():
 		self._play_command_to_mp3(self.call)
 		time.sleep(1) # for noise check
 		for command in self.commands[category]:
-			if command=='call' : self._play_command_to_mp3(self.call)
-			elif command.split(' ')[0]=='delay': time.sleep(int(command.split(' ')[1]))
-			else: self._play_command_to_mp3(command)
+			if command=='call' : 
+				self._play_command_to_mp3(self.call)
+			elif command.split(' ')[0]=='delay': 
+				time.sleep(int(command.split(' ')[1]))
+			else: 
+				self._play_command_to_mp3(command)
 			time.sleep(1)
 	
 	def start_command(self, category, write_record):
@@ -57,7 +60,7 @@ class CommandSpeeker():
 		isfrist = True
 		if os.path.exists(record_file): isfrist = False
 		with open(record_file, 'a') as file:
-			if isfrist: file.write('speeker\tcategory\ttime\n')
+			if isfrist: file.write('speaker\tcategory\ttime\n')
 			file.write(self.speaker+'\t'+category+'\t'+str(duration)+'\n')
 
 
@@ -65,12 +68,12 @@ if __name__=="__main__":
 	import argparse
 	parser = argparse.ArgumentParser()
 
-	parser.add_argument('-s', '--speeker',
-						help='voice assistance speeker type',
+	parser.add_argument('-s', '--speaker',
+						help='voice assistance speaker type',
 						type=str,
 						required=True)
 	parser.add_argument('-j', '--json-path',
-						help='the path of json file has speeker information',
+						help='the path of json file has speaker information',
 						type=str,
 						required=True)
 	parser.add_argument('-c', '--command-json-path',
@@ -87,6 +90,6 @@ if __name__=="__main__":
 						default=False)
 	ARGS = parser.parse_args()
 
-	commandspeeker = CommandSpeeker(ARGS.speeker, ARGS.json_path, ARGS.command_json_path)
-	commandspeeker.start_command(ARGS.function, ARGS.write_record)
+	commandspeaker = CommandSpeaker(ARGS.speaker, ARGS.json_path, ARGS.command_json_path)
+	commandspeaker.start_command(ARGS.function, ARGS.write_record)
 
