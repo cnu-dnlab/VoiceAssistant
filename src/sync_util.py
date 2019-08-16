4d5c94d1ee01a908402f611ce0ff110dbcf6fcae
munhyunsu <munhyunsu@gmail.com>
2019-08-16 14:12:43 +0900

    sync
    Signed-off-by: munhyunsu <munhyunsu@gmail.com>

--- src/sync_util.py
+++ src/sync_util.py
@@ -4,6 +4,7 @@ import os
 def sync_a_b(a_path, b_path):
     a_mtime = os.path.getmtime(a_path)
     b_mtime = os.path.getmtime(b_path)
-    #return a_mtime-b_mtime
-    return 4.205428558682638
+    return a_mtime-b_mtime
+    #return 4.205428558682638
+    #return 0
 
